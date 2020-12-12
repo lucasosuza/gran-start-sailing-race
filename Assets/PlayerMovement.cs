26a875d9e162ad1ca3e3fd96a61b373b13751b74
Lucas Souza <lucas.souza@diymaxeda.com>
2020-12-12 12:20:06 +0100

    Simple update

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] GameObject gb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gb = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("horizontal"))
+        { 
+            
+        }
+    }
+}
