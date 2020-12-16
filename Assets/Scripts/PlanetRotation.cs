f5c94917a5a74c20fa7abbf279b0cc7bff639f38
Lucas Souza <lucas.souza@diymaxeda.com>
2020-12-16 20:02:29 +0100

    Added first planet iteraction

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public float rotationSpeed = 10.0f;
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        //transform.localScale = Vector3.one * 1.3f;
+
+        Material material = Renderer.material;
+
+        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+    }
+
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
